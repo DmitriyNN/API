79bf5c3224ac27bb1f65b7e82f67c89b6d7324f8
Dimannn <diman1992nn@gmail.com>
2023-03-08 18:25:14 +0300

    add currency RUB instead of RUR

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-import static org.hamcrest.Matchers.equalTo;
 
 class MobileBankApiTestV4 {
     @Test
@@ -19,7 +18,6 @@ class MobileBankApiTestV4 {
       // Проверки
       .then()
           .statusCode(200)
-          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-      ;
+          .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
